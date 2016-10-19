d1c490ca579b513e2a501e428932db7d3c54bacb
Sam Krew <semen.krivosheev@gmail.com>
2016-10-19 13:13:40 +0200

    Added proxy settings to config

--- namecheap/config.js
+++ namecheap/config.js
@@ -4,9 +4,14 @@
     var lodash = require('lodash'),
         
         config = {},
+        proxy = null,
         requiredProperties = ['ApiUser', 'ApiKey', 'UserName', 'ClientIp'];
     
     function set(configName, configValue) {
+        if (configName === "Proxy") {
+            proxy = configValue;
+            return;
+        }
         
         if (requiredProperties.indexOf(configName) === -1) {
             throw new Error("That is not a configurable property.");
@@ -14,16 +19,7 @@
         if (!lodash.isString(configValue) || configValue.length === 0) {
             throw new Error("Configurable property must have a string value."); 
         }
-        
-        if (['ApiUser', 'UserName'].indexOf(configName) > -1) {
-            if (configName === "ApiUser" && !config.UserName) {
-                config.UserName = configValue;
-            }
-            if (configName === "UserName" && !config.ApiUser) {
-                config.ApiUser = configValue;   
-            }
-        }
-        
+
         config[configName] = configValue;
     }
     
@@ -33,6 +29,10 @@
     
     function getAll() {
         return lodash.clone(config);   
+    }
+
+    function getProxy() {
+        return proxy;
     }
     
     function isSatisfied() {
@@ -45,6 +45,7 @@
         set: set,
         get: get,
         getAll: getAll,
+        getProxy: getProxy,
         isSatisfied: isSatisfied
     };
     
